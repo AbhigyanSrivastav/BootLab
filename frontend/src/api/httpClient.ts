d3d0479c5db10453a24bb41abddc35e6540cc4b2
abhigyan <abhigyansrivastav7@gmail.com>
2025-06-22 18:48:37 +0530

    update

--- frontend/src/api/httpClient.ts
+++ frontend/src/api/httpClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const url = `http://13.204.46.236:5000/api`; //dev 
+// const url = `http://65.0.91.137:5000/api`; //dev 
 const url = `/api`; //proxy
 
 const httpClient = axios.create({
