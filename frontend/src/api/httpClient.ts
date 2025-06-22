21af7d175a740a936e1fa0962e2730c4ee13005f
abhigyan <abhigyansrivastav7@gmail.com>
2025-06-22 11:48:10 +0530

    setup proxy

--- frontend/src/api/httpClient.ts
+++ frontend/src/api/httpClient.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = "/api"; //proxy
+// const url = `${import.meta.env.VITE_API_BASE_URL || ""}/api`; //proxy
+// const url = `http://3.108.53.210:5000/api`; //proxy
+const url = `/api`; //proxy
 
 const httpClient = axios.create({
     baseURL: url,
